0cb6b11c89d7ad2cd517fc83109dd961a6a1a9a2
Branice Simaloi <simaloibranice@gmail.com>
2025-11-04 14:41:13 +0300

    Embed bot data directly for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,29 @@ import "./App.css";
 import botData from "./db.json";
 
 function App () {
-    const[bots, setBots] = useState(botData.bots);
+    const botData = [
+       {
+            "id": 101,
+            "name": "wHz-93",
+            "health": 90,
+            "damage": 20,
+            "armor": 10,
+            "bot_class": "Support",
+            "catchphrase": "1010010101001101100011000111101",
+            "avatar_url": "https://robohash.org/nostrumrepellendustenetur.png?size=300x300&set=set1"
+        },
+        {
+            "id": 102,
+            "name": "RyM-66",
+            "health": 86,
+            "damage": 36,
+            "armor": 77,
+            "bot_class": "Medic",
+            "catchphrase": "0110011100000100011110100110011000011001",
+            "avatar_url": "https://robohash.org/quidemconsequaturaut.png?size=300x300&set=set1"
+        }
+    ]
+    const[bots, setBots] = useState(botData);
     const [army, setArmy] = useState([]);
    
 
